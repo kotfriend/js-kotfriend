93fd05bac42f7d6e6a51490740d249191e9bc881
kotfriend <eugene.shulzhenko@gmail.com>
2022-09-17 21:04:19 +0300

    homework

---
+++ src/index.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// const SKY_COLOR = "blue";
+
+// const a = 5;
+// const b = 10;
+// const x = a + b;
+
+// // console.log("Hello world");
+// // console.log("x:", x);
+
+// // // alert("ddffd");
+
+// // function hello() {
+// //   console.log("Hello world");
+// //   console.log(this);
+// // }
+
+// // hello();
+// // hello();
+// // hello();
+// // hello();
+// // hello();
+
+// const userName = "Bobby";
+// const userAge = 15;
+// const userAdult = false;
+// const userEye = "blue";
+
+// console.log("User Name:", userName);
+// console.log(typeof userName);
+// console.log(typeof userAge);
+// console.log(typeof userAdult);
+// console.log(typeof { x: 121212 });
+
+// // console.log("g:", g);
+// // var g = 2;
+// // console.log("g:", g);
+
+// // console.log(2 > 3);
+// // console.log(4 < 5);
+
+// const goToTheMovie = confirm("Do you want go to the Movie");
+// console.log("Answer:", goToTheMovie);
+
+const pressAge = prompt("Please press your Age:");
+console.log("Answer from User:", pressAge);
+
+if (pressAge > 18) {
+  console.log("You can buy ticcket");
+} else {
+  console.log("You can go home and go to sleep");
+}
